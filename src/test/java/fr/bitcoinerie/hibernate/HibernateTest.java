0ca13ace8b3c2eb45b1c3a7f9392a59d7b30a132
Bach PhanLuong <bach.phan-luong@centrale-marseille.fr>
2013-12-04 16:37:21 +0100

    Commit 4 - 12 -13, JUNIT ongoing

---
+++ src/test/java/fr/bitcoinerie/hibernate/HibernateTest.java
@@ -0,0 +1,11 @@
+package fr.bitcoinerie.hibernate;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bphan-luong
+ * Date: 04/12/13
+ * Time: 16:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class HibernateTest {
+}
